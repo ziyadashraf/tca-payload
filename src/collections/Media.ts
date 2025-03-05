fef26ee6f3233a26cb7d2b8d81fc8adab17d0de7
zeyadsallam <ziyadazab36@gmail.com>
2025-03-05 15:34:51 +0200

    auto generate alt

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -13,7 +13,24 @@ export const Media: CollectionConfig = {
       name: 'alt',
       type: 'text',
       required: true,
+      admin: {
+        hidden: true,
+      },
     },
   ],
   upload: true,
+  hooks: {
+    beforeChange: [
+      ({ data }) => {
+        if (data?.filename) {
+          // Generate alt text from filename
+          data.alt = data.filename
+            .replace(/\.[^/.]+$/, '') // Remove file extension
+            .replace(/-/g, ' ') // Replace hyphens with spaces
+            .replace(/\b\w/g, (l: string) => l.toUpperCase()) // Capitalize first letter of each word
+        }
+        return data
+      },
+    ],
+  },
 }
