849a973c9693429ad1f98282c65f0c1267d0d00b
zeyadsallam <ziyadazab36@gmail.com>
2025-03-05 17:10:43 +0200

    e

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@payloadcms/storage-s3/client': '@payloadcms/storage-s3/dist/client',
-    }
-    return config
-  },
+  // Your Next.js config here
 }
 
 export default withPayload(nextConfig)
