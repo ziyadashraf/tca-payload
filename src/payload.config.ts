3aab79c4726065d9e994619a61f9745c329eb1a9
zeyadsallam <ziyadazab36@gmail.com>
2025-05-12 13:23:10 +0300

    j

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -38,18 +38,16 @@ export default buildConfig({
     'https://www.admin.tca.com.sa',
     'https://tca.com.sa',
     'https://www.tca.com.sa',
-    '*',
   ],
-  csrf: [
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'https://tca-kappa.vercel.app',
-    'https://admin.tca.com.sa',
-    'https://www.admin.tca.com.sa',
-    'https://tca.com.sa',
-    'https://www.tca.com.sa',
-    '*',
-  ],
+  // csrf: [
+  //   'http://localhost:3000',
+  //   'http://localhost:3001',
+  //   'https://tca-kappa.vercel.app',
+  //   'https://admin.tca.com.sa',
+  //   'https://www.admin.tca.com.sa',
+  //   'https://tca.com.sa',
+  //   'https://www.tca.com.sa',
+  // ],
   serverURL:
     process.env.ENVIRONMENT === 'development'
       ? 'http://localhost:3001'
